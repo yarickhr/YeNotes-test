71810d43a13d841278276e8f74290362a2f97827
iliana <iliana000@gmail.com>
2019-02-28 13:04:32 +0200

    build

--- build/precache-manifest.b56a16beec8e90a84e052f772d1bd87f.js
+++ build/precache-manifest.b56a16beec8e90a84e052f772d1bd87f.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/YeNotes-test/build/static/js/runtime~main.8a850066.js"
   },
   {
-    "revision": "2d1d6e8227a109eded5b",
-    "url": "/YeNotes-test/build/static/js/main.2d1d6e82.chunk.js"
+    "revision": "0e749e1786a880f27e42",
+    "url": "/YeNotes-test/build/static/js/main.0e749e17.chunk.js"
   },
   {
     "revision": "3a4dbb340a59a7e800f1",
     "url": "/YeNotes-test/build/static/js/2.3a4dbb34.chunk.js"
   },
   {
-    "revision": "2d1d6e8227a109eded5b",
+    "revision": "0e749e1786a880f27e42",
     "url": "/YeNotes-test/build/static/css/main.8385ab5f.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/YeNotes-test/build/static/css/2.7f5c6357.chunk.css"
   },
   {
-    "revision": "371637f9c8fdade7f375b93c92644331",
+    "revision": "f9f24637d03c6017cf0e76a345d49c2a",
     "url": "/YeNotes-test/build/index.html"
   }
 ];
